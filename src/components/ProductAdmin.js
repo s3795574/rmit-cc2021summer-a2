733585a6d4b96e0114b3ef8da766cf544c30f25b
Man Hou <s3795574@student.rmit.edu.au>
2021-01-26 16:51:03 +1100

    upload pic function done.

--- src/components/ProductAdmin.js
+++ src/components/ProductAdmin.js
@@ -7,13 +7,47 @@ export default class ProductAdmin extends Component {
   state = {
     etag:"",
     filename:"",
-    downloadLink:""
+    downloadLink:"",
+    secureUploadLink:"",
+    key:"",
+    image:""
+  }
+  uploadFile = async (event) =>{
+    event.preventDefault();
+    try{
+      const res = await axios.get(`${config.S3api.invokeUrl}/s3/${this.props.auth.user.username}?filename=${this.state.filename}`);
+      const data = res.data;
+      this.setState({secureUploadLink:data.uploadURL});
+      this.setState({key:data.Key});
+      console.log(this.state.secureUploadLink);
+      console.log(this.state.key);
+
+      var image = document.getElementById('uploadFile').files[0];
+
+      const response = await fetch(
+        new Request(this.state.secureUploadLink, {
+          method: 'PUT',
+          body: image,
+          headers: new Headers({
+            'Content-Type': 'image/jpeg',
+            "Access-Control-Allow-Origin":"*"
+          }),
+        }),
+      );
+
+      console.log(response);
+      console.log(image);
+
+    }catch(err){
+      console.log(`An error has occurred: ${err}`);
+    }  
   }
 
   addingDynamoDBRecord = async (user,etag,filename,metaData, event) => {
     event.preventDefault();
     // add call to AWS API Gateway add product endpoint here
     try {
+      //This object represent a record in DynamoDB
       const params = {
         "user": user,
         "etag": etag,
@@ -30,6 +64,7 @@ export default class ProductAdmin extends Component {
   updatingDynamoDBRecord = async (user,etag,filename,metaData, name) => {
     // add call to AWS API Gateway update product endpoint here
     try {
+      //This object represent a record in DynamoDB
       const params = {
         "user": user,
         "etag": etag,
@@ -54,8 +89,8 @@ export default class ProductAdmin extends Component {
   //because the setState and console.log are not asynchronous, so we use the callback function as a secondary argument
   //So that we can debug in browser console. This method will set filename once we select a file
   onChange = async (event) => {
-    this.setState({filename:event.target.files[0].name},()=>{
-      console.log(this.state.filename)
+    this.setState({filename:event.target.files[0].name.split('.')[0]},()=>{
+      console.log(this.state.filename.split('.')[0])
     }) 
     
   }
@@ -67,11 +102,10 @@ export default class ProductAdmin extends Component {
     return (
       <Fragment>
        <section className="section">
-          <form onSubmit={event => this.addingDynamoDBRecord(
-            this.props.auth.user.username,this.state.etag,this.state.filename,this.state.downloadLink, event)}>
+          <form onSubmit={event => this.uploadFile(event)}>
             <h1>File Upload</h1>
             <p className="subtitle is-5">Add a file to your cloud storage:</p>
-            <input type="file" onChange={this.onChange} />
+            <input type="file" id="uploadFile" onChange={this.onChange} />
             <button type="submit">Upload</button>
           </form>
         </section>
